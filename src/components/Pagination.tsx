640ab87a9fa78d5be76d0c0938f3d3e04084c65f
Gabriel Fresatto <gabrielfresatto@gmail.com>
2026-01-18 18:52:59 -0300

    feat: changing the pagination color based on app theme

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,32 +1,31 @@
-import { View } from "react-native";
-import Animated from "react-native-reanimated";
-import { twMerge } from "tailwind-merge";
+import { View } from 'react-native'
+import Animated from 'react-native-reanimated'
+import { twMerge } from 'tailwind-merge'
 
 type PaginationProps = {
-  total: number;
-  current: number;
-};
+  total: number
+  current: number
+}
 
 export const Pagination = ({ total, current }: PaginationProps) => {
-  const dots = Array.from({ length: total });
+  const dots = Array.from({ length: total })
 
   return (
-    <View className="flex-row gap-2">
+    <View className='flex-row gap-2'>
       {dots.map((_, index) => (
         <Animated.View
           key={index}
           className={twMerge(
-            // TODO: Change color when the current theme change
-            "w-2 h-2 rounded-full bg-gray-400",
-            current === index && "w-8"
+            'w-2 h-2 rounded-full bg-contrast-text',
+            current === index && 'w-8'
           )}
           style={{
             width: current === index ? 24 : 8,
-            transitionProperty: "width",
-            transitionDuration: 200,
+            transitionProperty: 'width',
+            transitionDuration: 200
           }}
         />
       ))}
     </View>
-  );
-};
+  )
+}
