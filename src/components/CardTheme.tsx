3c363d9c794a8e94ee4c31cdb50d53999abab470
Gabriel Fresatto <gabrielfresatto@gmail.com>
2026-01-18 14:21:49 -0300

    feat: removing unused vars and configuring prettier

--- src/components/CardTheme.tsx
+++ src/components/CardTheme.tsx
@@ -1,63 +1,54 @@
-import { ColorValue, Text, View } from "react-native";
-import { AppThemes } from "../themes";
-import { RoundedCardBackgorund } from "./RoundedCardBackground";
-import { tv } from "tailwind-variants";
-import { colors } from "../themes/colors";
+import { Text, View } from 'react-native'
+import { AppThemes } from '../themes'
+import { RoundedCardBackgorund } from './RoundedCardBackground'
+import { tv } from 'tailwind-variants'
+import { useThemeGradient } from '../hooks/useThemeGradient'
 
 type CardThemeProps = {
-  theme: AppThemes;
-  name: string;
-  description: string;
-};
+  theme: AppThemes
+  name: string
+  description: string
+}
 
 const variants = tv({
   slots: {
-    root: "w-full h-full border rounded-3xl p-4 justify-between",
-    headerText: "text-gray-500 text-sm",
-    name: "text-contrast-text text-3xl font-bold",
-    description: "text-contrast-text text-sm",
+    root: 'w-full h-full border rounded-3xl p-4 justify-between',
+    headerText: 'text-gray-500 text-sm',
+    name: 'text-contrast-text text-3xl font-bold',
+    description: 'text-contrast-text text-sm'
   },
   variants: {
     theme: {
       charcoal: {
-        root: "border-charcoal-contrast",
-        headerText: "text-charcoal-contrast-text",
-        name: "text-charcoal-contrast-text text-3xl font-bold",
-        description: "text-charcoal-contrast-text text-sm",
+        root: 'border-charcoal-contrast',
+        headerText: 'text-charcoal-contrast-text',
+        name: 'text-charcoal-contrast-text text-3xl font-bold',
+        description: 'text-charcoal-contrast-text text-sm'
       },
       winter: {
-        root: "border-winter-contrast",
-        headerText: "text-winter-contrast-text",
-        name: "text-winter-contrast-text text-3xl font-bold",
-        description: "text-winter-contrast-text text-sm",
+        root: 'border-winter-contrast',
+        headerText: 'text-winter-contrast-text',
+        name: 'text-winter-contrast-text text-3xl font-bold',
+        description: 'text-winter-contrast-text text-sm'
       },
       night: {
-        root: "border-night-contrast",
-        headerText: "text-night-contrast-text",
-        name: "text-night-contrast-text text-3xl font-bold",
-        description: "text-night-contrast-text text-sm",
-      },
-    },
-  },
-});
+        root: 'border-night-contrast',
+        headerText: 'text-night-contrast-text',
+        name: 'text-night-contrast-text text-3xl font-bold',
+        description: 'text-night-contrast-text text-sm'
+      }
+    }
+  }
+})
 
 export const CardTheme = ({ theme, name, description }: CardThemeProps) => {
-  const styles = variants({ theme });
-
-  const { charcoal, winter, night } = colors;
-
-  const gradients: Record<AppThemes, [ColorValue, ColorValue]> = {
-    charcoal: [charcoal.primary, charcoal.secondary],
-    winter: [winter.primary, winter.secondary],
-    night: [night.primary, night.secondary],
-  };
-
-  const [from, to] = gradients[theme];
+  const styles = variants({ theme })
+  const { from, to } = useThemeGradient(theme)
 
   return (
     <RoundedCardBackgorund colors={[from, to]}>
       <View className={styles.root()}>
-        <View className="flex-row items-center justify-between">
+        <View className='flex-row items-center justify-between'>
           <Text className={styles.headerText()}>01</Text>
           <Text className={styles.headerText()}>Theme 1</Text>
         </View>
@@ -67,5 +58,5 @@ export const CardTheme = ({ theme, name, description }: CardThemeProps) => {
         </View>
       </View>
     </RoundedCardBackgorund>
-  );
-};
+  )
+}
